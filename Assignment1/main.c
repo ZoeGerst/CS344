d9c3368c37458b1ab79d430075bb5183e1162705
ZoeGerst <81997340+ZoeGerst@users.noreply.github.com>
2022-10-07 01:14:08 -0700

    Add files via upload
    Movies first draft

---
+++ Assignment1/main.c
@@ -0,0 +1,26 @@
+//Zoe Gerst
+//Assignment 1: Movies
+//10/10/2022
+//CS344
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct movie{
+
+	int year;
+	double rating;
+	char *title;
+	struct movie *next;
+
+}
+
+
+
+int main(int argc, char *argv[]){
+
+
+
+}
