8510fd306a75c9330575967b36f2fd391c0df7ff
ZoeGerst <81997340+ZoeGerst@users.noreply.github.com>
2022-10-07 14:03:43 -0700

    Add files via upload
    createMovie

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -13,13 +13,33 @@ struct movie{
 	int year;
 	double rating;
 	char *title;
+	char language;
 	struct movie *next;
 
 }
 
 struct movie *createMovie(char *currLine){
 
-	
+	struct movie *currMovie = malloc(sizeof(struct movie));
+	char *saveptr;
+
+	char *token = strtok_r(currLine, " ", &saveptr);
+	currMovie->title = calloc(strlen(token) + 1, sizeof(char));
+    strcpy(currMovie->title, token);
+
+	token = strtok_r(NULL, " ", &saveptr);
+    currMovie->year = atoi(token);
+
+	token = strtok_r(NULL, " ", &saveptr);
+	currMovie->language = calloc(strlen(token) + 1, sizeof(char));
+	strcpy(currMovie->language, token);
+
+	token = strtok_r(NULL, "\n", &saveptr);
+	char *ptr;
+	currMovie->rating = strtod(token, &ptr);
+
+	currMovie->next = NULL
+	return currMovie;
 
 }
 
