46af166d199edceb9115fc19dc04a87b04fde763
ZoeGerst <81997340+ZoeGerst@users.noreply.github.com>
2022-11-17 02:17:11 -0800

    Add files via upload
    building off of while loops

--- Assignment4/myCounter.c
+++ Assignment4/myCounter.c
@@ -7,6 +7,15 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#define SIZE 10
+
+int myCond1; //empty
+int myCond2; //full
+int myCount = 0;
+int buffer[10];
+
+pthread_mutex_t myMutex
+
 
 
 
@@ -15,19 +24,19 @@ void producer(){
 
 	while(1){
 
-		pthread_mutex_lock(&mutex);
+		pthread_mutex_lock(&myMutex);
 
-		while (count == SIZE){
+		while (myCount == SIZE){
 
-			pthread_cond_wait(&space_available, &mutex);
+			pthread_cond_wait(&myCond1, &myMutex);
 
 		}
 
 		put_item(value);
 
-		pthread_mutex_unlock(&mutex);
+		pthread_mutex_unlock(&myMutex);
 
-		pthread_cond_signal(&has_data); 
+		pthread_cond_signal(&myCond2); 
 
 	}
 
@@ -39,19 +48,19 @@ void consumer(){
 
 	while(1){
 
-		pthread_mutex_lock(&mutex);
+		pthread_mutex_lock(&myMutex);
 
-		while (count == 0){
+		while (myCount == 0){
 
-			pthread_cond_wait(&has_data, &mutex);
+			pthread_cond_wait(&myCond2, &myMutex);
 
 		}
 
 		value = get_item();
 
-		pthread_mutex_unlock(&mutex);
+		pthread_mutex_unlock(&myMutex);
 
-		pthread_cond_signal(&space_available);
+		pthread_cond_signal(&myCond1);
 
 	}
 
@@ -62,5 +71,14 @@ void consumer(){
 int main(){
 
 
+
+	printf("PROGRAM START\n");
+
+
+
+
+	printf("PROGRAM END\n");
+
+	return 0;
 
 }
