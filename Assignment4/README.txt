620a385028fb5463d7a05aea02d09596822cdcd5
ZoeGerst <81997340+ZoeGerst@users.noreply.github.com>
2022-11-21 17:42:35 -0800

    Add files via upload
    Comments and README

---
+++ Assignment4/README.txt
@@ -0,0 +1,5 @@
+
+
+gcc --std=gnu99 -pthread -o myCounter myCounter.c
+
+//try it a few times for different outputs
--- Assignment4/myCounter.c
+++ Assignment4/myCounter.c
@@ -1,4 +1,12 @@
-
+//Zoe Gerst
+//11/21/2022
+//CS 344
+//Assignment 4: Multi-threaded producer consumer counter
+//Works cited:
+//https://code-vault.net/lesson/tlu0jq32v9:1609364042686
+//https://stackoverflow.com/questions/40855584/c-producer-consumer-using-pthreads
+//https://www.geeksforgeeks.org/multithreading-c-2/
+//https://www.geeksforgeeks.org/producer-consumer-problem-in-c/
 
 
 
@@ -8,16 +16,18 @@
 #include <assert.h>
 #include <time.h>
 
-#define SIZE 2
+#define SIZE 5 //max threads
 
 pthread_cond_t myCond1; //empty
 pthread_cond_t myCond2; //full
-int myCount = 0;
+int myCount = 0; //counter
 int buffer[10];
-int counter = 0;
+//int counter = 0;
 
-pthread_mutex_t myMutex;
+pthread_mutex_t myMutex; //mutex
 
+
+//puts item in thread
 void put_item(int value){
 
 	myCount++;
@@ -27,6 +37,7 @@ void put_item(int value){
 
 }
 
+//gets item from thread
 void get_item(){
 
 
@@ -43,26 +54,35 @@ void get_item(){
 void *producer(){
 
 	int value = 0;
-	printf("CONSUMER THREAD CREATED\n");
+	printf("CONSUMER THREAD CREATED\n"); //only way to get it on the second line
 
+	//gives thread a stopping point
 	while(myCount < 10){
 
+		//locks mutex
 		pthread_mutex_lock(&myMutex);
 
 		printf("PRODUCER: myMutex is locked\n");
 
+		//uses a buffer while waiting
 		while (myCount == SIZE){
 
-			pthread_cond_wait(&myCond1, &myMutex);
-
 			printf("PRODUCER: waiting on myCond1\n");
+			pthread_cond_wait(&myCond1, &myMutex);
+		//	printf("Test producer\n");
 
 		}
+
+		//increases myCount
 		printf("myCount: %d -> ", myCount);
 
 		put_item(value);
 
 		printf("%d\n", myCount);
+
+
+	//	printf("PRODUCER: waiting on myCond1\n");
+	//	pthread_cond_wait(&myCond1, &myMutex);
 
 		//if(myCount == 10){
 
@@ -74,16 +94,21 @@ void *producer(){
 //		myCount++;
 
 //		printf("myCount: %d -> %d\n", value, myCount);
+//
+//		printf("PRODUCER: signaling myCond2\n");
+//		pthread_cond_signal(&myCond2);
 
+		//unlocks mutex
 		pthread_mutex_unlock(&myMutex);
 
 		printf("PRODUCER: myMutex is unlocked\n");
 
+		//signals myCond2
+		printf("PRODUCER: signaling myCond2\n");
 		pthread_cond_signal(&myCond2);
 
-		printf("PRODUCER: signaling myCond2\n");
 
-		counter++;
+//		counter++;
 	}
 
 }
@@ -93,35 +118,48 @@ void *consumer(){
 
 	int value;
 
+	//had to make this < 8, because it kept printing 10 -> 11 for 9 and 10
 	while(myCount < 8){
 
+	//	printf("CONSUMER: waiting on myCond2\n");
+	//	pthread_cond_wait(&myCond2, &myMutex);
+
+		//locks mutex
 		pthread_mutex_lock(&myMutex);
 
 		printf("CONSUMER: myMutex is locked\n");
 
+		//if buffer is empty
 		while (myCount == 0){
 
-			pthread_cond_wait(&myCond2, &myMutex);
-
 			printf("CONSUMER: waiting on myCond2\n");
+			pthread_cond_wait(&myCond2, &myMutex);
+		//	printf("Test consumer\n");
+
 
 		}
 
+		//gets item
 		printf("myCount: %d -> ", myCount);
 
 		get_item();
 
 		printf("%d\n", myCount);
 
+		//printf("CONSUMER: waiting on myCond2\n");
+		//pthread_cond_wait(&myCond2, &myMutex);
+
+		//unlocks mutex
 		pthread_mutex_unlock(&myMutex);
 
 		printf("CONSUMER: myMutex is unlocked\n");
 
+		//signals myCond1
+		printf("CONSUMER: signaling myCond1\n");
 		pthread_cond_signal(&myCond1);
 
-		printf("CONSUMER: signaling myCond1\n");
 
-		counter++;
+	//	counter++;
 	}
 
 }
@@ -132,10 +170,12 @@ int main(){
 
 	srand(time(NULL));
 
+	//number of threads
 	pthread_t PRO[SIZE];
 
 	int x, y;
 
+	//gives buffer
 	for(int i = 0; i < 10; i++){
 
 		buffer[i] = -1;
@@ -144,10 +184,12 @@ int main(){
 
 	printf("PROGRAM START\n");
 
+	//creates threads
 	x = pthread_create(&PRO[0], NULL, producer, NULL);
 	y = pthread_create(&PRO[1], NULL, consumer, NULL);
 
 
+	//joins threads
 	pthread_join(PRO[0], NULL);
 	pthread_join(PRO[1], NULL);
 
