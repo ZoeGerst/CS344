342c8db574b49db69494da304583eb2ee78846da
ZoeGerst <81997340+ZoeGerst@users.noreply.github.com>
2022-12-02 03:40:16 -0800

    Add files via upload
    debugging keygen

--- Assignment5/keygen.c
+++ Assignment5/keygen.c
@@ -16,16 +16,20 @@ int main(int ardc, char argv[]){
 
 	if(argc != 2){
 
-		
+		fprintf(stderr, "Correct length of key is not given\n");
+		exit(0);
 
 	}
 
 	int keylength = atoi(argv[1]);
+	char finalKey[atoi(argv[1])];
+	memset(finalKey, '\0', sizeof(finalKey));
 	char randArr;
 
 	if(keylength <= 0){
 
-		
+		fprintf(stderr, "Keylength must be greater than 0\n");
+		exit(0);
 
 	}
 
@@ -34,10 +38,13 @@ int main(int ardc, char argv[]){
 	for(int i = 0; i < keylength; i++){
 
 		randArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "[rand() % 27];
+		finalKey[i] = randArr;
 
 	}
 
+	finalKey[keylength] = '\0';
 
+	printf("%s\n", finalKey);
 
 	return 0;
 }
