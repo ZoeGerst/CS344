0d90dcc898e434eea6e7f7704efe17e6c5855c27
ZoeGerst <81997340+ZoeGerst@users.noreply.github.com>
2022-12-07 20:58:32 -0800

    Add files via upload
    comments

--- Assignment5/keygen.c
+++ Assignment5/keygen.c
@@ -1,4 +1,9 @@
-
+//Zoe Gerst
+//12/7/2022
+//CS344
+//Assignment 5: One-Time Pads: keygen.c
+////Works Cited:
+//https://replit.com/@cs344/81dnsbyname#main.c
 
 
 #include <stdio.h>
@@ -13,7 +18,7 @@
 int main(int argc, char* argv[]){
 
 
-
+	//Must be the correct length
 	if(argc != 2){
 
 		fprintf(stderr, "Correct length of key is not given\n");
@@ -23,6 +28,8 @@ int main(int argc, char* argv[]){
 
 //	char *pointer;
 //	pointer = &argv[1];
+//
+//	user input
 
 	int keylength = atoi(argv[1]);
 	char finalKey[atoi(argv[1])];
@@ -38,8 +45,10 @@ int main(int argc, char* argv[]){
 
 	}
 
+	//random time
 	srand(time(NULL));
 
+	//creates a random set of characters
 	for(int i = 0; i < keylength; i++){
 
 		randArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "[rand() % 27];
